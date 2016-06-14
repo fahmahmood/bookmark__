3d7eda7d9fd8b0e798ef86437b7ef68ed9e49a73
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-14 15:49:07 +0100

    added tags

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
-require_relative 'models/link'
-
+# require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   
@@ -21,8 +21,12 @@ class BookmarkManager < Sinatra::Base
 	end 
 
 	post '/links' do
-		Link.create(url: params[:url], title: params[:title])
-		redirect '/links'
+	 link = Link.new(url: params[:url],     # 1. Create a link
+                title: params[:title])
+	  tag  = Tag.first_or_create(name: params[:tags])  # 2. Create a tag for the link
+	  link.tags << tag                       # 3. Adding the tag to the link's DataMapper collection.
+	  link.save                              # 4. Saving the link.
+	  redirect to('/links')
 	end 
 
   # start the server if ruby file executed directly
--- app/models/link.rb
+++ app/models/link.rb
@@ -5,15 +5,18 @@ class Link
 
 include DataMapper::Resource
 
+has n, :tags, through: Resource
+
 property :id, Serial
 property :title, String
 property :url, String
 
+
 end 
 
-# Now let's set up a connection with a database
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-# Let's check that everything we wrote in our models was OK
-DataMapper.finalize
-# And let's build any new columns or tables we added
-DataMapper.auto_upgrade!
+# # Now let's set up a connection with a database
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+# # Let's check that everything we wrote in our models was OK
+# DataMapper.finalize
+# # And let's build any new columns or tables we added
+# DataMapper.auto_upgrade!
