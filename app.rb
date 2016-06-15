bf69b8c01776152c9d13bbd688213079f20cf504
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-15 16:19:17 +0100

    added tags and filters

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require './models/link'
+require_relative 'data_mapper_setup'
 
 class Bookmarks < Sinatra::Base
 
@@ -18,9 +19,19 @@ class Bookmarks < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.create(url: params[:url], title: params[:title]) #create a link
+    tag = Tag.first_or_create(name: params[:tags]) #create tag for that link
+    link.tags << tag  #add the tag to the collection of tags (that you already have)
+    link.save #save th elink
     redirect '/links'
   end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
 
   run! if app_file == $0
 end
--- models/link.rb
+++ models/link.rb
@@ -2,10 +2,11 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 
-
 class Link
 
   include DataMapper::Resource
+
+  has n, :tags, through: Resource
 
   property :id,     Serial
   property :title,  String
@@ -13,8 +14,8 @@ class Link
 
 end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
-DataMapper.finalize
+# DataMapper.finalize
 
-DataMapper.auto_upgrade!
+# DataMapper.auto_upgrade!
